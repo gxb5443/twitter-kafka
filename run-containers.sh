828dde04a6046821f8f398bbd3462a7962c633b4
Gian Biondi <gian.biondi@doubleverify.com>
2017-01-27 16:05:02 -0500

    add docker

---
+++ run-containers.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+function dockerInstalled() {
+  if ! command -v docker &> /dev/null ; then
+    echo "Docker not installed"
+    exit 1
+  fi
+}
+
+function sbtInstalled() {
+  if ! command -v sbt &> /dev/null ; then
+    echo "SBT not installed"
+    exit 1
+  fi
+}
+
+function clearContainers() {
+  docker rm -f ingest
+  docker rm -f aggregation
+  docker rm -f kafka
+  docker rm -f zookeeper
+}
+
+function clearImages() {
+  docker rmi -f twitterstream_ingest
+  docker rmi -f twitterstream_aggregation
+}
+
+dockerInstalled
+sbtInstalled
+
+sbt twitterstream/assembly
+
+clearContainers
+clearImages
+
+cp application.conf docker/ingest/
+cp twitterstream/target/scala-2.12/twitterstream.jar docker/ingest/twitterstream.jar
+cp application.conf docker/aggregation/
+cp twitterstream/target/scala-2.12/twitterstream.jar docker/aggregation/twitterstream.jar
+
+docker-compose up --force-recreate
